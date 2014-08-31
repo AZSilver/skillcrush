88c28ded1584a05dad61d31e472005497bcbfd82
Andrea <andreasmitha2@gmail.com>
2014-08-31 12:15:47 -0700

    my first commit

---
+++ always_three.rb
@@ -0,0 +1,3 @@
+puts "give me a number"
+a = gets.chomp
+puts ((((a.to_i + 5) * 2) - 4) / 2) - a.to_i 
---
+++ case.rb
@@ -0,0 +1,19 @@
+puts "whats your favorite color?"
+fav_color = gets.chomp
+
+case fav_color
+when 'red'
+puts "Red like fire!"
+when 'orange'
+puts "Orange like an orange."
+when 'yellow'
+puts "Like the yellow sun!"
+when 'green'
+puts "Like the green earth!"
+when 'blue'
+puts "Blue like the water!"
+when 'silver'
+puts "Just like Silverman!"
+else 
+puts "Hmm, well I don't know what color that color is!"
+end
